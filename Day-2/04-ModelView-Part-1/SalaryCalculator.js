2cfe61ace56ce224c3e38fd3e445b972111fc251
tkmagesh <tkmagesh77@gmail.com>
2014-10-07 14:50:10 +0530

    Model-View - Part - 2

--- Day-2/04-ModelView-Part-1/SalaryCalculator.js
+++ Day-2/04-ModelView-Part-1/SalaryCalculator.js
@@ -1,12 +1,38 @@
-function SalaryCalculator(basic,hra,da,tax){
-    this.basic = basic;
-    this.hra = hra;
-    this.da = da;
-    this.tax = tax;
-    this.salary = 0;
+function SalaryCalculator(defaults){
+    var data = defaults || {
+        basic : 0,
+        hra : 0,
+        da : 0,
+        tax : 0,
+        salary : 0
+    };
+    var triggerChange = (function(attrName){
+        var subscriptionFns = subscribers[attrName] || [];
+        for(var i=0;i<subscriptionFns.length;i++)
+            subscriptionFns[i].call(this);
+    }).bind(this);
+
+    this.get = function(attrName){
+        return data[attrName];
+    };
+    this.set = function(attrName,value){
+        data[attrName] = value;
+        triggerChange(attrName);
+    }
+
+    var subscribers = {};
+    this.addSubscriber = function(attrName,subscriptionFn){
+        subscribers[attrName] = subscribers[attrName] || [];
+        subscribers[attrName].push(subscriptionFn);
+    };
+    this.removeSubscriber = function(attrName,subsciptionFn){
+        //fill in the blanks
+    };
 
 }
 SalaryCalculator.prototype.calculate = function(){
-    var gross = this.basic + this.hra + this.da;
-    this.salary = gross * ((100-this.tax)/100);
+    var gross = this.get("basic") + this.get("hra") + this.get("da");
+    var net = gross * ((100-this.get("tax"))/100);
+    this.set('salary',net);
+
 }
