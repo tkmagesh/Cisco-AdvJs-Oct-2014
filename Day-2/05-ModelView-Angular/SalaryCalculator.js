a9780e7422f3fab5808eba59257ba168d22a265f
tkmagesh <tkmagesh77@gmail.com>
2014-10-07 15:44:10 +0530

    Model-View - Complete

---
+++ Day-2/05-ModelView-Angular/SalaryCalculator.js
@@ -0,0 +1,13 @@
+function SalaryCalculator(){
+    this.basic = 0;
+    this.hra = 0;
+    this.da = 0;
+    this.tax = 0;
+    this.salary = 0;
+
+}
+SalaryCalculator.prototype.calculate = function(){
+    var gross = parseInt(this.basic,10) + parseInt(this.hra,10) + parseInt(this.da,10);
+    var net = gross * ((100-parseInt(this.tax,10))/100);
+    this.salary = net;
+}
